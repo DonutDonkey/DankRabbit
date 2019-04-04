d545147b2d2fcd208c524fc7e76b82b0e6621a10
KonradGM <34782950+DonutDonkey@users.noreply.github.com>
2019-04-04 12:53:05 +0200

    4.4.2019
    -compressed fiels to fit within size limit
    -finished first level

---
+++ Assets/Scripts/Objects/LevelEnd.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelEnd : MonoBehaviour
+{
+    [SerializeField]
+    private string loadLevel;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            SceneManager.LoadScene(loadLevel);
+        }
+    }
+}
